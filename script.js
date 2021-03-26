1bccc36f61e6cb2ca553a916bb53c4dd9fa3c7a9
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-03-26 15:16:10 -0600

    Styling using Bootstrap and CSS.

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ class App extends React.Component{
     render(){
         return(
             <div className="container" id="drum-machine">
-                <div id="display">
-                    <Pad/>
-                </div>
+                <Pad/>
             </div>
         )
     }
@@ -15,16 +13,40 @@ class App extends React.Component{
 
 const Pad=(props)=>{
     return(
-        <div className="drumpad">
-            <button className="drum-pad" id="Q">Q</button>
-            <button className="drum-pad" id="W">W</button>
-            <button className="drum-pad" id="E">E</button>
-            <button className="drum-pad" id="A">A</button>
-            <button className="drum-pad" id="S">S</button>
-            <button className="drum-pad" id="D">D</button>
-            <button className="drum-pad" id="Z">Z</button>
-            <button className="drum-pad" id="X">X</button>
-            <button className="drum-pad" id="C">C</button>
+        <div className="container p-3 bg-dark border border-secondary rounded" id="display">
+            <div className="row">
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="Q">Q</button>
+                </div>
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="W">W</button>
+                </div>
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="E">E</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="A">A</button>
+                </div>
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="S">S</button>
+                </div>
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="D">D</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="Z">Z</button>
+                </div>
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="X">X</button>
+                </div>
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="C">C</button>
+                </div>
+            </div>
         </div>
     )
 }
