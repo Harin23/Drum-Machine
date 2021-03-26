8dc9e4a83e6d55bc98003f71f8033a261ded01fe
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-03-26 15:34:31 -0600

    Task 4: Embedding Audio Elements into the pad

--- script.js
+++ script.js
@@ -16,35 +16,44 @@ const Pad=(props)=>{
         <div className="container p-3 bg-dark border border-secondary rounded" id="display">
             <div className="row">
                 <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="Q">Q</button>
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="Q">Q<audio className="clip" id="Q" src="./Damn-Son.mp3"></audio></button>
                 </div>
                 <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="W">W</button>
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="W">W<audio className="clip" id="W" src="./Damn-Son.mp3"></audio></button>
                 </div>
                 <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="E">E</button>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="A">A</button>
-                </div>
-                <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="S">S</button>
-                </div>
-                <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="D">D</button>
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="E">E<audio className="clip" id="E" src="./Damn-Son.mp3"></audio></button>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="Z">Z</button>
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="A">A<audio className="clip" id="A" src="./Damn-Son.mp3"></audio></button>
                 </div>
                 <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="X">X</button>
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="S">S<audio className="clip" id="S" src="./Damn-Son.mp3"></audio></button>
                 </div>
                 <div className="col">
-                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" id="C">C</button>
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="D">D<audio className="clip" id="D" src="./Damn-Son.mp3"></audio></button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="Z">Z<audio className="clip" id="Z" src="./Damn-Son.mp3"></audio></button>
+                </div>
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="X">X<audio className="clip" id="X" src="./Damn-Son.mp3"></audio></button>
+                </div>
+                <div className="col">
+                    <button className="drum-pad btn btn-secondary border border-dark rounded w-100 m-1" 
+                    id="C">C<audio className="clip" id="C" src="./Damn-Son.mp3"></audio></button>
                 </div>
             </div>
         </div>
