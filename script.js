a7f1ffbb5090678c7eead1894c478fe77c91b9b7
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-03-26 14:07:28 -0600

    task 3 completed

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ class App extends React.Component{
         return(
             <div className="container" id="drum-machine">
                 <div id="display">
-                    
+                    <button className="drum-pad" id="Q">Q</button>
+                    <button className="drum-pad" id="W">W</button>
+                    <button className="drum-pad" id="E">E</button>
+                    <button className="drum-pad" id="A">A</button>
+                    <button className="drum-pad" id="S">S</button>
+                    <button className="drum-pad" id="D">D</button>
+                    <button className="drum-pad" id="Z">Z</button>
+                    <button className="drum-pad" id="X">X</button>
+                    <button className="drum-pad" id="C">C</button>
                 </div>
             </div>
         )
